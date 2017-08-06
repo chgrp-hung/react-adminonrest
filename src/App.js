979286a7e1e1cc50cf37a08725603ecf45e0a3ec
chgrp-hung <hung@thechgrp.com>
2017-08-06 04:48:47 -0700

    post edit and create

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate} from './posts';
 import { UserList } from './users';
 
 // an Admin component can have one or more Resource compoents
 const App = () => (
     <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
         <Resource name="users" list={UserList} />
     </Admin>
 );
