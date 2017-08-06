5652242396a4fb6138b46d5172a69a305f692d9f
chgrp-hung <hung@thechgrp.com>
2017-08-06 03:50:35 -0700

    post -> users reference added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// in src/App.js
+import React from 'react';
+import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { PostList } from './posts';
+import { UserList } from './users';
+
+// an Admin component can have one or more Resource compoents
+const App = () => (
+    <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+        <Resource name="posts" list={PostList} />
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
 
 export default App;
