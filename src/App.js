96be1f62c57b863ddb68f12f09082d597c742a9b
chgrp-hung <hung@thechgrp.com>
2017-08-09 02:54:34 -0700

    custom rest client

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import authClient from './authClient';
 import Dashboard from './dashboard';
 import { SimplePostList, ResponsivePostList, PostList, PostEdit, PostCreate} from './posts';
 import { UserList } from './users';
-
+import myApiRestClient from './restClient';
 
 // an Admin component can have one or more Resource compoents
 const App = () => (
-    <Admin authClient={authClient} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+    <Admin authClient={authClient} restClient={myApiRestClient}>
         <Resource name="posts" list={ResponsivePostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon} />
     </Admin>
 );
 
-
+//Dashboard version
 const AppDash = () => (
   <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
 
